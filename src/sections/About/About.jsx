9be145d556c3ba9a9b9cae1161cfbd1456d57693
Ashley Chu <ashleymchu@yahoo.com>
2021-02-18 09:19:32 -0800

    holder launch

---
+++ src/sections/About/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// class About extends React.Component{
+//
+//
+//     render() {
+//         return(
+//           <h1>Test</h1>
+//
+//
+//         );
+//     }
+// }
+
+function About() {
+    return(
+
+        <h1>Hello world</h1>
+    );
+}
+
+export default About;
---
+++ src/sections/LazyLoadSections.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import About from './About/About'
+
+const LazyLoadedSections = () => (
+    <>
+        <About></About>
+    </>
+);
+
+export default LazyLoadedSections;
